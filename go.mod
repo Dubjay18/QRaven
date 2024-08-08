6ed94536bd5f7309cc616d626c5a19d553e0553b
Dubjay18 <jejeniyi7@gmail.com>
2024-08-08 17:22:44 +0100

    chore: server init

--- go.mod
+++ go.mod
@@ -8,8 +8,14 @@ require (
 )
 
 require (
-	github.com/cespare/xxhash/v2 v2.1.2 // indirect
+	github.com/beorn7/perks v1.0.1 // indirect
+	github.com/cespare/xxhash/v2 v2.2.0 // indirect
 	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
+	github.com/patrickmn/go-cache v2.1.0+incompatible // indirect
+	github.com/prometheus/client_model v0.5.0 // indirect
+	github.com/prometheus/common v0.48.0 // indirect
+	github.com/prometheus/procfs v0.12.0 // indirect
+	golang.org/x/time v0.5.0 // indirect
 )
 
 require (
@@ -17,6 +23,7 @@ require (
 	github.com/bytedance/sonic/loader v0.2.0 // indirect
 	github.com/cloudwego/base64x v0.1.4 // indirect
 	github.com/cloudwego/iasm v0.2.0 // indirect
+	github.com/didip/tollbooth v4.0.2+incompatible
 	github.com/fsnotify/fsnotify v1.7.0 // indirect
 	github.com/gabriel-vasile/mimetype v1.4.5 // indirect
 	github.com/gin-contrib/sse v0.1.0 // indirect
@@ -43,8 +50,10 @@ require (
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/modern-go/reflect2 v1.0.2 // indirect
 	github.com/pelletier/go-toml/v2 v2.2.2 // indirect
+	github.com/prometheus/client_golang v1.19.1
 	github.com/sagikazarmark/locafero v0.4.0 // indirect
 	github.com/sagikazarmark/slog-shim v0.1.0 // indirect
+	github.com/sirupsen/logrus v1.9.3
 	github.com/sourcegraph/conc v0.3.0 // indirect
 	github.com/spf13/afero v1.11.0 // indirect
 	github.com/spf13/cast v1.6.0 // indirect
---
+++ pkg/router/router.go
@@ -0,0 +1,58 @@
+package router
+
+import (
+	"net/http"
+	"qraven/internal/config"
+	"qraven/pkg/middleware"
+	"qraven/pkg/repository/storage"
+	"qraven/utils"
+
+	"github.com/gin-gonic/gin"
+	"github.com/go-playground/validator/v10"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+)
+
+func Setup(logger *utils.Logger, validator *validator.Validate, db *storage.Database, appConfiguration *config.App) *gin.Engine {
+	if appConfiguration.Mode == "release" {
+		gin.SetMode(gin.ReleaseMode)
+	}
+	r := gin.New()
+
+	// Middlewares
+	// r.Use(gin.Logger())
+	r.ForwardedByClientIP = true
+	r.SetTrustedProxies(config.GetConfig().Server.TrustedProxies)
+	r.Use(middleware.Security())
+	r.Use(middleware.Logger())
+	r.Use(gin.Recovery())
+	r.Use(middleware.CORS())
+	r.Use(middleware.Metrics(config.GetConfig()))
+	// r.Use(middleware.GzipWithExclusion("/metrics"))
+	r.MaxMultipartMemory = 3 << 20
+
+	// routers
+	ApiVersion := "api/v1"
+	api := r.Group(ApiVersion)
+	api.GET("/", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"status_code": 200,
+			"message":     "Welcome to QRaven API",
+			"status":      http.StatusOK,
+		})
+	})
+
+
+	r.NoRoute(func(c *gin.Context) {
+		c.JSON(http.StatusNotFound, gin.H{
+			"name":        "Not Found",
+			"message":     "Page not found.",
+			"status_code": 404,
+			"status":      http.StatusNotFound,
+		})
+	})
+
+	// Prometheus metrics endpoint
+	r.GET("/metrics", gin.WrapH(promhttp.Handler()))
+
+	return r
+}
